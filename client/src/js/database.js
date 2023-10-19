c3402a6fb8f45be7f4986a9ff2656e3d587e2f17
EleazarF <eleazar.fuentes@gmail.com>
2023-10-18 19:41:32 -0600

    Update database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,42 +1,37 @@
 import { openDB } from 'idb';
 
-const initdb = async () => {
- await openDB('jateDB', 1, {
+const initdb = async () =>
+  openDB('jate', 1, {
     upgrade(db) {
       if (db.objectStoreNames.contains('jate')) {
-        console.log('jate store already exists');
+        console.log('jate database already exists');
         return;
       }
       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate store created');
+      console.log('jate database created');
     },
   });
-  console.log('DB OPEN');
-};
 
-// Added logic to a method that accepts some content and adds it to the database
+// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT request sent to the jateDB');
-  const jateDb = await openDB('jateDB', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
-  const request = store.put({ id: 1, value: content });
+  const txtEditorDB = await openDB('jate', 1);
+  const transVar = txtEditorDB.transaction('jate', 'readwrite');
+  const storeVar = transVar.objectStore('jate');
+  const request = storeVar.put({ id: 1, value: content });
+
   const result = await request;
-  console.log('Data successfully saved to jate', result);
+  console.log('Data saved to database', result.value);
 };
 
-// Added logic for a method that gets all the content from the database
-
+// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  // console.error('getDb not implemented');
-  console.log('GET all from the jateDB');
-  const jateDb = await openDB('jateDB', 1);
-  const tx = jateDb.transaction('jate', 'readonly');
-  const store = tx.objectStore('jate');
-  const request = store.get(1);
+  const txtEditorDB = await openDB('jate', 'readonly');
+  const storeVar = trasnVar.objectStore('jate');
+  const request = storeVar.get(1);
   const result = await request;
-  console.log('Data successfully pulled from jateDB', result);
-  return result?.value;
+  result
+    ? console.log('Data retrieved from the database', result.value)
+    : console.log('Data not found in the database');
 };
 
 initdb();
