4e16041a835b604565b3a15793527a0499c083e0
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 10:09:04 -0600

    Update install.js

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,21 +1,33 @@
+// Get the 'Install' button element by its ID
 const butInstall = document.getElementById("buttonInstall");
 
-window.addEventListener("beforeinstallprompt", (event) => {
-  window.deferredPrompt = event
-  butInstall.classList.toggle("hidden", false)
-})
-
-butInstall.addEventListener("click", async () => {
-  const promptEvent = window.deferredPrompt;
-  if (!promptEvent) {
-    return;
-  }
-  promptEvent.prompt();
-  window.deferredPrompt = null;
-  butInstall.classList.toggle("hidden", true);
+// Listen for the 'beforeinstallprompt' event, triggered when the app is installable
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log('beforeinstallprompt event triggered');
+    event.preventDefault();
+    // Store the event for deferred installation
+    window.deferredPrompt = event;
+    // Display the 'Install' button by removing the 'hidden' class
+    butInstall.classList.toggle('hidden', false);
 });
 
-window.addEventListener("appinstalled", (event) => {
-  window.deferredPrompt = null
-  console.log("👍", "appinstalled", event);
+// Add a click event listener to the 'Install' button
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        return; // Do nothing if there is no deferred prompt
+    }
+    // Show the installation prompt
+    promptEvent.prompt();
+    // Reset the deferred prompt variable as it can be used only once
+    window.deferredPrompt = null;
+    // Hide the 'Install' button by adding the 'hidden' class
+    butInstall.classList.toggle('hidden', true);
+});
+
+// Listen for the 'appinstalled' event, triggered when the app is successfully installed
+window.addEventListener('appinstalled', (event) => {
+    // Clear the deferred prompt as the app has been successfully installed
+    console.log('App installed successfully');
+    window.deferredPrompt = null;
 });
