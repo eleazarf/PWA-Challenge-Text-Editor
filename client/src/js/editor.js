7096a515c79a4d71780a5c1ec11d856f596b4417
EleazarF <eleazar.fuentes@gmail.com>
2023-10-19 10:06:14 -0600

    Update editor.js

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -1,16 +1,21 @@
-// Import methods to save and get data from the indexedDB database in './database.js'
+// Import methods from './database.js' for saving and retrieving data from IndexedDB
 import { getDb, putDb } from './database';
+
+// Import the 'header' content from './header.js'
 import { header } from './header';
 
-export default class {
+// Define a class for the text editor
+export default class TextEditor {
   constructor() {
+    // Retrieve content from local storage
     const localData = localStorage.getItem('content');
 
-    // check if CodeMirror is loaded
+    // Check if CodeMirror is loaded
     if (typeof CodeMirror === 'undefined') {
       throw new Error('CodeMirror is not loaded');
     }
 
+    // Create a CodeMirror editor instance
     this.editor = CodeMirror(document.querySelector('#main'), {
       value: '',
       mode: 'javascript',
@@ -22,18 +27,18 @@ export default class {
       tabSize: 2,
     });
 
-    // When the editor is ready, set the value to whatever is stored in indexeddb.
-    // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
+    // Load data from IndexedDB if available; otherwise, fallback to local storage or 'header' content
     getDb().then((data) => {
       console.info('Loaded data from IndexedDB, injecting into editor');
       this.editor.setValue(data || localData || header);
     });
 
+    // Save the editor content to local storage when it changes
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor to IndexedDB when the editor loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
